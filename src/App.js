27d3dd32ce0422b1a83b718901d911ad57f21ec4
bejibay <aykayode@gmail.com>
2023-06-21 20:18:09 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] =useState([
+    {
+      data: "Build To Do App With React",
+      isCompleted: false
+    },
+    {
+      data: "Build LMS With Laravel",
+      isCompleted: false
+    },
+    {
+      data: "Build Shopping Cart with React Redux",
+      isCompleted: false
+    }
+  ]);
+
+  const addAction = data => {
+    const newTodos = [...todos, { data }];
+    setTodos(newTodos);
+  };
+
+  const completeAction = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeAction = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="action-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} completeAction={completeAction} removeAction={removeAction} />
+        ))}
+        <AddTodoForm addAction={addAction} />
+      </div>
     </div>
   );
 }
 
-export default App;
+
+
+
+function Todo({ todo, index, completeAction, removeAction }) {
+  return (
+    <div
+      className="todo"
+      style={{ backgroundColor: todo.isCompleted ? "lightblue" : "" }}
+    >
+      {todo.data}
+      <div>
+        <button onClick={() => completeAction(index)}>Complete</button>
+        <button onClick={() => removeAction(index)}>x</button>
+      </div>
+    </div>
+  );
+}
+
+function AddTodoForm({ addAction }) {
+  const [value, setValue] =useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addAction(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)}/>
+      <input type ="submit" value ="Add New Todo"/>
+    </form>
+  );
+}
+
